15a9a7612d78d11d832d9144c84cd2941785d3fb
FrejaIBredahl <201904700@post.au.dk>
2022-03-08 14:21:55 +0100

    Add project files.

---
+++ ModelManagementAssignment2/Data/ModelManagementDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelManagementAssignment2.Models;
+
+namespace ModelManagementAssignment2.Data
+{
+    public class ModelManagementDb : DbContext 
+    {
+        public ModelManagementDb(DbContextOptions<ModelManagementDb> options) : base(options) { }
+
+        public DbSet<Model> Models => Set<Model>(); 
+
+        public DbSet<Job> Jobs => Set<Job>();
+
+        public DbSet<Expense> Expenses => Set<Expense>();
+    }
+}
